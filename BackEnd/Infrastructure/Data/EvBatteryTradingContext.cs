a5aa14fd805ff7b6e3ce41bf081d5330407ffb1d
Tecookie <quanglikecookie@gmail.com>
2025-10-01 19:11:37 +0700

    refractor: delete unrelated content

--- BackEnd/Infrastructure/Data/EvBatteryTradingContext.cs
+++ BackEnd/Infrastructure/Data/EvBatteryTradingContext.cs
@@ -51,7 +51,7 @@ public class EvBatteryTradingContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=EV_Battery_Trading;User ID=sa;Password=12345;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=TECOOKIE\\SQLEXPRESS;Initial Catalog=EV_Battery_Trading;User ID=sa;Password=123456;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
