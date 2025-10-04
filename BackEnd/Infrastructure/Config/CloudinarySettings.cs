05ae222aa565aa50c6f1a43274a3c715e44ed8a8
TUNAPI <tugia24052004@gmail.com>
2025-10-05 01:34:08 +0700

    fix: user secret

--- BackEnd/Infrastructure/Config/CloudinarySettings.cs
+++ BackEnd/Infrastructure/Config/CloudinarySettings.cs
@@ -8,8 +8,8 @@ namespace Infrastructure.Config
 {
     public class CloudinarySettings
     {
-        public string CloudName { get; set; } = string.Empty;
-        public string ApiKey { get; set; } = string.Empty;
-        public string ApiSecret { get; set; } = string.Empty;
+        //public string CloudName { get; set; } = string.Empty;
+        //public string ApiKey { get; set; } = string.Empty;
+        //public string ApiSecret { get; set; } = string.Empty;
     }
 }
