2a92c88fa3460091a28133e235b765d1ed43c93d
KIETTA-TAK <trananhkiet21082005@gmail.com>
2025-10-12 23:00:37 +0700

    Update EVDetail.cs

--- BackEnd/Domain/Entities/EVDetail.cs
+++ BackEnd/Domain/Entities/EVDetail.cs
@@ -4,47 +4,32 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
-    [Table("ev_detail")]
     public class EVDetail
     {
-        [Key]
-        [Column("item_id")]
         public int ItemId { get; set; }
 
-        [Column("brand")]
         public string Brand { get; set; }
 
-        [Column("model")]
         public string Model { get; set; }
 
-        [Column("version")]
         public string Version { get; set; }
 
-        [Column("year")]
         public int? Year { get; set; }
 
-        [Column("body_style")]
         public string BodyStyle { get; set; }
 
-        [Column("color")]
         public string Color { get; set; }
 
-        [Column("license_plate")]
         public string LicensePlate { get; set; }
 
-        [Column("has_accessories")]
         public bool? HasAccessories { get; set; }
 
-        [Column("previous_owners")]
         public int? PreviousOwners { get; set; }
 
-        [Column("is_registration_valid")]
         public bool? IsRegistrationValid { get; set; }
 
-        [Column("mileage")]
         public int? Mileage { get; set; }
 
-        [Column("updated_at")]
-        public DateTime UpdatedAt { get; set; }
+        public DateOnly UpdatedAt { get; set; }
     }
 }
