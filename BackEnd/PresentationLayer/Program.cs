01161c56c6490b2cbd9dd7dcd2d692eae4d50d06
kiet-ta <trananhkiet21082005@gmail.com>
2025-10-15 23:52:02 +0700

    feat(wallet): create api wallet controller

--- BackEnd/PresentationLayer/Program.cs
+++ BackEnd/PresentationLayer/Program.cs
@@ -39,7 +39,6 @@ namespace PresentationLayer
             builder.Services.AddDbContext<EvBatteryTradingContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.Configure<FirebaseOptions>(builder.Configuration.GetSection("Firebase"));
 
-
             // DI for Repository + Service
             //---Services
             builder.Services.AddScoped<IAuthService, AuthService>();
@@ -77,7 +76,7 @@ namespace PresentationLayer
             builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
             builder.Services.AddScoped<IItemImageRepository, ItemImageRepository>();
 
-            // AddHttp 
+            // AddHttp
             builder.Services.AddHttpClient<IChatRepository, FirebaseChatRepository>();
             builder.Services.AddHttpContextAccessor();
 
@@ -178,7 +177,7 @@ namespace PresentationLayer
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Configuration.AddUserSecrets<Program>();
-            
+
             builder.Services.AddScoped<IMailService, MailService>();
             builder.Services.AddScoped<IEmailRepository, EmailTemplateRepository>();
             builder.Services.AddScoped<IValidator<PaymentRequestDto>, PaymentRequestValidator>();
@@ -188,6 +187,7 @@ namespace PresentationLayer
             builder.Services.AddScoped<IFavoriteRepository, FavoriteRepository>();
             builder.Services.AddScoped<IKYC_DocumentRepository, KYC_DocumentRepository>();
             builder.Services.AddAutoMapper(typeof(KYC_DocumentProfile).Assembly);
+            builder.Services.AddScoped<IWalletService, WalletService>();
             //builder.Services.AddSwaggerGen();
 
             builder.Services.AddSwaggerGen(c =>
