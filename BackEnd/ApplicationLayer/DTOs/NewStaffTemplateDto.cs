9a5e7f5a9b12df6e8fa2204f48f4f72274995535
ottise <quynchinguyen.010205@gmail.com>
2025-10-20 12:56:39 +0700

    feat: send temporaty password for new staff

---
+++ BackEnd/ApplicationLayer/DTOs/NewStaffTemplateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class NewStaffTemplateDto
+    {
+        public required string To { get; set; }
+        public string ActionUrl { get; set; } = "#";
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; }
+        public string LogoUrl { get; set; }
+
+    }
+}
