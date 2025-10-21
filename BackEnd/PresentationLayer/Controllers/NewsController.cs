bb24ebbee10415d3e227aba769e3158a15fb4b29
ottise <quynchinguyen.010205@gmail.com>
2025-10-22 00:08:19 +0700

    refactor: refactor news controller and news service

--- BackEnd/PresentationLayer/Controllers/NewsController.cs
+++ BackEnd/PresentationLayer/Controllers/NewsController.cs
@@ -59,24 +59,14 @@ namespace PresentationLayer.Controllers
             await _notificationService.RegisterClientAsync(Response, ct, userId);
             Console.WriteLine($"User {userId} subscribed for SSE.");
 
-            try
+
+            while (!ct.IsCancellationRequested)
             {
-                while (!ct.IsCancellationRequested)
-                {
-                    await Task.Delay(TimeSpan.FromSeconds(15), ct);
-                    await Response.WriteAsync(":\n\n"); 
-                    await Response.Body.FlushAsync();
-                }
+                await Task.Delay(TimeSpan.FromSeconds(15), ct);
+                await Response.WriteAsync(":\n\n");
+                await Response.Body.FlushAsync();
             }
-            catch (TaskCanceledException)
-            {
-            }
-            finally
-            {
-                await _notificationService.UnRegisterClientAsync(Response);
-                Console.WriteLine($"User {userId} unsubscribed.");
-            }
-            
+
         }
         [HttpPost]
         public async Task<IActionResult> AddNews([FromBody] CreateNewsDto dto)
