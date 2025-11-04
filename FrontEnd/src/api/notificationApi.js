eb7968104adcc47940a271ee913eb8f46fa377e7
ottise <quynchinguyen.010205@gmail.com>
2025-11-04 23:23:39 +0700

    fix: fix notification

--- FrontEnd/src/api/notificationApi.js
+++ FrontEnd/src/api/notificationApi.js
@@ -1,17 +1,17 @@
 import axios from "axios";
-const baseURL = import.meta.env.VITE_API_BASE_URL + "Notifications";
+const baseURL = import.meta.env.VITE_API_BASE_URL + "notification";
 const notificationApi = {
-    getNotificationByReceiverId: async (userId) => {
-        const response = await axios.get(`${baseURL}/receiver/${userId}`);
-        return response.data;
-    },
-    createNotification: async (payload) => {
-        const response = await axios.post(`${baseURL}`,payload)
-        return response.data;
-    },
-    putNotificationStatusIsRead: async (notificationId) => {
-        const response = await axios.put(`${baseURL}/${notificationId}/read`)
-        return response.data;
-    }
+  getNotificationByReceiverId: async (userId) => {
+    const response = await axios.get(`${baseURL}/receiver/${userId}`);
+    return response.data;
+  },
+  createNotification: async (payload) => {
+    const response = await axios.post(`${baseURL}`, payload);
+    return response.data;
+  },
+  putNotificationStatusIsRead: async (notificationId) => {
+    const response = await axios.put(`${baseURL}/${notificationId}/read`);
+    return response.data;
+  },
 };
 export default notificationApi;
