3098631ff9f1827cbc1cd6cbafef55dea05bf59d
TuCore <tugia24052004@gmail.com>
2025-11-26 17:51:42 +0700

    fix dev branch

--- BackEnd/Infrastructure/Repositories/PaymentDetailRepository.cs
+++ BackEnd/Infrastructure/Repositories/PaymentDetailRepository.cs
@@ -102,6 +102,7 @@ namespace Infrastructure.Repositories
             _context.Update(paymentDetail);
             await _context.SaveChangesAsync();
             return paymentDetail;
+        }
         public async Task CreatePaymentDetailAsync(PaymentDetail paymentDetail)
         {
             await _context.PaymentDetails.AddAsync(paymentDetail);
