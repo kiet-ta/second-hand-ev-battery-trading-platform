53078e7d401e332aae5ca142ad0b41cef3b81311
TuCore <tugia24052004@gmail.com>
2025-11-27 16:30:38 +0700

    fix: api with-draw wallet

--- BackEnd/ApplicationLayer/Validations/PaymentRequestValidator.cs
+++ BackEnd/ApplicationLayer/Validations/PaymentRequestValidator.cs
@@ -8,7 +8,7 @@ public class PaymentRequestValidator : AbstractValidator<PaymentRequestDto>
     public PaymentRequestValidator()
     {
         RuleFor(x => x.UserId).NotEmpty();
-        RuleFor(x => x.Method).Must(m => m == "Wallet" || m == "payos");
+        RuleFor(x => x.Method).Must(m => m == "Wallet" || m == "PayOS");
         RuleFor(x => x.TotalAmount).GreaterThan(0).Must((req, total) => total == req.Details.Sum(d => d.Amount)).WithMessage("Total details do not match");
     }
 }
